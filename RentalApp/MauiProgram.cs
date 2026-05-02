5c933660275bc293db02b93161d89be24f0c326b
crob03 <40683354@live.napier.ac.uk>
2026-05-02 19:00:21 +0000

    refactor: replace IApiService/IAuthenticationService registrations with four domain service pairs

--- RentalApp/MauiProgram.cs
+++ RentalApp/MauiProgram.cs
@@ -1,4 +1,3 @@
-// RentalApp/MauiProgram.cs
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using RentalApp.Database.Data;
@@ -33,26 +32,28 @@ public static class MauiProgram
             var baseAddress = new Uri("https://set09102-api.b-davison.workers.dev/");
             builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = baseAddress });
             builder.Services.AddSingleton<IApiClient, ApiClient>();
-            builder.Services.AddSingleton<IApiService, RemoteApiService>();
+            builder.Services.AddSingleton<IAuthService, RemoteAuthService>();
+            builder.Services.AddSingleton<IItemService, RemoteItemService>();
+            builder.Services.AddSingleton<IRentalService, RemoteRentalService>();
+            builder.Services.AddSingleton<IReviewService, RemoteReviewService>();
         }
         else
         {
             builder.Services.AddDbContextFactory<AppDbContext>();
             builder.Services.AddSingleton<IItemRepository, ItemRepository>();
             builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
-            builder.Services.AddSingleton<IApiService>(sp => new LocalApiService(
+            builder.Services.AddSingleton<IAuthService>(sp => new LocalAuthService(
                 sp.GetRequiredService<IDbContextFactory<AppDbContext>>(),
+                sp.GetRequiredService<AuthTokenState>()
+            ));
+            builder.Services.AddSingleton<IItemService>(sp => new LocalItemService(
                 sp.GetRequiredService<IItemRepository>(),
                 sp.GetRequiredService<ICategoryRepository>(),
                 sp.GetRequiredService<AuthTokenState>()
             ));
+            builder.Services.AddSingleton<IRentalService, LocalRentalService>();
+            builder.Services.AddSingleton<IReviewService, LocalReviewService>();
         }
-
-        builder.Services.AddSingleton<IAuthenticationService>(sp => new AuthenticationService(
-            sp.GetRequiredService<IApiService>(),
-            sp.GetRequiredService<ICredentialStore>(),
-            sp.GetRequiredService<AuthTokenState>()
-        ));
 
         builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
         builder.Services.AddSingleton<ILocationService, LocationService>();
